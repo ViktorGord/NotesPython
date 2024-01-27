8dd848aa6164106950699db35263373e9c337b02
Viktor Gordikov <flaiho@bk.ru>
2024-01-27 20:36:18 +0300

    final

--- NotesPython.py
+++ NotesPython.py
@@ -3,7 +3,6 @@
 import sqlite3 as sql
 from tkinter import *
 from tkinter import messagebox
-# Create database connection and connect to table
 try:
        con = sql.connect('gorwin.db')
        cur = con.cursor()
@@ -21,8 +20,8 @@ def add_notes():
                messagebox.showerror(message = "Просьба ввести корректные данные" )
        else:
        #Insert into the table
-               cur.execute("Вставить в notes_table VALUES ('%s','%s','%s')" %(today, notes_title, notes))
-               messagebox.showinfo(message="Note added")
+               cur.execute("INSERT INTO notes_table VALUES ('%s','%s','%s')" %(today, notes_title, notes))
+               messagebox.showinfo(message="Заметка добавлена")
                con.commit() #Комитим
  #ОТОБРАЖЕНИЕ ЗАМЕТОК
 def view_notes():
@@ -53,7 +52,7 @@ def delete_notes():
        #Удаляем все заметки
        choice = messagebox.askquestion(message="Вы хотите удалить все заметки?")
        if choice == 'yes':
-               sql_statement = "Удалить из notes_table" 
+               sql_statement = "DELETE FROM notes_table" 
        else:
        #Удаляем заметки по названию и дате
                if (len(date) <=0) & (len(notes_title)<=0): 
@@ -61,9 +60,9 @@ def delete_notes():
                        messagebox.showerror(message = "Просьба ввести корректные данные" )
                        return
                else:
-                      sql_statement = "Удалить из notes_table where date ='%s' and notes_title ='%s'" %(date, notes_title)
+                      sql_statement = "DELETE FROM notes_table where date ='%s' and notes_title ='%s'" %(date, notes_title)
        cur.execute(sql_statement)
-       messagebox.showinfo(message="Note(s) Deleted")
+       messagebox.showinfo(message="Заметки удалены")
        con.commit()
 #ОБНОВЛЕНИЕ ЗАМЕТОК
 def update_notes():
@@ -75,9 +74,36 @@ def update_notes():
                messagebox.showerror(message = "Просьба ввести корректные данные" )
        #update the note
        else:
-               sql_statement = "Обновить notes_table SET notes = '%s' where date ='%s' and notes_title ='%s'" %(notes, today, notes_title)
+               sql_statement = "UPDATE notes_table SET notes = '%s' where date ='%s' and notes_title ='%s'" %(notes, today, notes_title)
               
        cur.execute(sql_statement)
        messagebox.showinfo(message="Заявка обновлена")
        con.commit()              
-       
+       #Invoke call to class to view a window
+window = Tk()
+#Разрабатываем GUI
+window.geometry("500x300")
+window.title("Gorwin Notes")
+#Заголовок окна программы & наименование, расположение и размер поля ввода даты 
+title_label = Label(window, text="GORWIN NOTES. Приложение для работы с заметками").pack()
+date_label = Label(window, text="Дата:").place(x=10,y=20)
+date_entry = Entry(window,  width=20)
+date_entry.place(x=50,y=20)
+#Наименование, расположение и размер поля для вовода название заметки 
+notes_title_label = Label(window, text="Название:").place(x=10,y=50)
+notes_title_entry = Entry(window,  width=30)
+notes_title_entry.place(x=80,y=50)
+#Наименование, расположение и размер поля для вовода текста заметки
+notes_label = Label(window, text="Текст:").place(x=10,y=90)
+notes_entry = Text(window, width=50,height=5)
+notes_entry.place(x=60,y=90)
+ 
+#Кнопки и их функционал
+button1 = Button(window,text='Добавить', bg = 'Turquoise',fg='Blue',command=add_notes).place(x=10,y=190)
+button2 = Button(window,text='Просмотр', bg = 'Turquoise',fg='Blue',command=view_notes).place(x=110,y=190)
+button3 = Button(window,text='Удалить', bg = 'Turquoise',fg='Blue',command=delete_notes).place(x=210,y=190)
+button4 = Button(window,text='Обновить', bg = 'Turquoise',fg='Blue',command=update_notes).place(x=320,y=190)
+ 
+#close the app
+window.mainloop()
+con.close()
